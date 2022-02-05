23efd3bdfd0f252d90099b9d580b7131df0ba9f1
KellyZhangL <zhangk@brandeis.edu>
2022-02-05 16:28:39 -0500

    Server setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,34 @@
-import Express from "express";
+//import dotenv from 'dotenv';
+import express from 'express';
+import { json } from 'body-parser';
 
-const app = Express();
-app.get("/", (req, res)=.)
-// GET hhtps://localhost:3000 
-// PUT, POST, DELETE
-app.get
-console.log("hi");
+(async () => {
+  const app = express()
+  const PORT = 5000;
+
+  app.use(json())
+
+  app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+  })
+})();
+
+
+// // load the environment variables from the .env file
+// dotenv.config({
+  // path: '.env'
+// });
+
+// /**
+ // * Express server application class.
+ // * @description Will later contain the routing system.
+ // */
+// class Server {
+  // public app = express();
+// }
+
+// // initialize server app
+// const server = new Server();
+
+// // make server listen on some port
+// server.app.use('/api', server.router);
